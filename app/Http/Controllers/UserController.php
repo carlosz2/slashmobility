3d8b6755195e2ada581bfbb9c12840fb79e36f6e
unknown <carlos.zambrano@ellecktra.com>
2021-12-18 19:50:14 -0300

    agregando controlador users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {        
+        // Validaciones para registrar un usuario
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|',            
+        ]);
+
+        // Creamos al usuario
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        //$user->password = $request->password; //sin hashing
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        // La API nos devuelve una respuesta
+        return response()->json([
+            "status" => 1,
+            "msg" => 'Alta de Usuario exitosa'
+        ]);
+
+    }
+
+    public function login(Request $request) {
+        // Validaciones para hacer el login
+        $request->validate([
+            'username'=>'required',
+            'email'=>'required|string|email|max:100|unique:users',
+            'password'=>'required|string|min:6',
+        ]);        
+        $user = User::where("email", "=", $request->email)->first();                
+        if(isset($user->id)){             
+            if(Hash::check($request->password, $user->password)) {                
+               $token = $user->createToken("auth_token")->plainTextToken;                                 
+                return response()->json([
+                    "status" => 1,
+                    "message" => "¡Usuario logueado exitosamente!",
+                    "access_token" => $token
+                ]);     
+            }else{                
+                return response()->json([
+                    "status" => 0,
+                    "message" => "La password es incorrecta"
+                ], 404);    
+            }            
+        }else{
+            return response()->json( [
+                "status" => 0,
+                "message" => "Usuario no registrado"
+            ], 404);
+        }        
+    }
+
+    public function userProfile() {        
+        return response()->json([
+            "status" => 1,
+            "message" => "Acerca del perfil de usuario",
+            "data" => auth()->user()
+        ]);
+    }
+    
+    public function logout() {        
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "status" => 1,
+            "message" => "Cierre de sesión OK"           
+        ]);
+    }
+}
